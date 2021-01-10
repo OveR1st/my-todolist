d9aca82b58e4f164efaa05ac74aa908fa0ab5b81
Dmitry <60567057+OveR1st@users.noreply.github.com>
2021-01-10 13:47:22 +0200

    initial commit

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,18 +1,20 @@
-import './Header.css';
+import React from "react";
+
+import "./Header.css";
 
 const Header = () => {
-  return(
+  return (
     <div className="header d-flex row justify-content-md-between align-items-center">
-        <div className="search-panel col-md-7">
-          <input type="text" className="form-control" placeholder="Search?"/>
-        </div>
-        <div className="button-Filter col-md-4 d-flex justify-content-end p-0">
-          <button className="btn btn-outline-primary mr-2">All</button>
-          <button className="btn btn-outline-primary mr-2">Active</button>
-          <button className="btn btn-outline-primary">Done</button>
-        </div>
+      <div className="search-panel col-md-7">
+        <input type="text" className="form-control" placeholder="Search?" />
       </div>
+      <div className="button-Filter col-md-4 d-flex justify-content-end p-0">
+        <button className="btn btn-outline-primary mr-2">All</button>
+        <button className="btn btn-outline-primary mr-2">Active</button>
+        <button className="btn btn-outline-primary">Done</button>
+      </div>
+    </div>
   );
-}
+};
 
 export default Header;
